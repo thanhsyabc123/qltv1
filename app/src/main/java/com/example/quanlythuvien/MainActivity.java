edb067b520a42c3a42a6d118aa1a730173ef2d84
laptrinhvien <doan111aaa@gmail.com>
2023-10-20 16:51:10 +0700

    push bai da lam xong

--- app/src/main/java/com/example/quanlythuvien/MainActivity.java
+++ app/src/main/java/com/example/quanlythuvien/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DemoDB demoDB=new DemoDB(getApplicationContext());
-
+//qưerwefdfrgv
     }
 }
